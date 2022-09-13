1afe78eea411923e8efec2ba0ce1f135c6226948
Bulbous Dreamer <89059638+bulbousdreamer@users.noreply.github.com>
2022-09-13 12:28:48 -0500

    rhel git_ext_diff: Remove cmp from script to see if it helps because the files display unintelligibly

--- .homer/rhel/bin/git_ext_diff.sh
+++ .homer/rhel/bin/git_ext_diff.sh
@@ -8,10 +8,4 @@ new_file="${5}"
 new_hex="${6}"
 new_mode="${7}"
 
-# If the files are the same, do not open them in the bc4 GUI
-# TODO test filemode too or know that this is for content-only diffs?
-if cmp --silent "${old_file}" "${new_file}"; then
-    exit 0
-fi
-
 vimdiff -c 'set wrap' -c 'wincmd w' -c 'set wrap'  "${old_file}" "${new_file}"
