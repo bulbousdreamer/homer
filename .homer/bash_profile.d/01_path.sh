81852155a2aa521cfd99da3a7766212853fd8c41
Bulbous Dreamer <89059638+bulbousdreamer@users.noreply.github.com>
2022-09-15 10:33:50 -0500

    Make OS specific bin appear first in PATH to override the general version

--- .homer/bash_profile.d/01_path.sh
+++ .homer/bash_profile.d/01_path.sh
@@ -2,5 +2,5 @@
 # This way OS's can use the generic script until customization
 # is necessary. Just create a script in their bin.
 echo "Doing the path"
-export PATH="${HOME}/.homer/${HOMER_OS_TYPE}/bin${PATH:+:${PATH}}"
 export PATH="${HOME}/.homer/bin${PATH:+:${PATH}}"
+export PATH="${HOME}/.homer/${HOMER_OS_TYPE}/bin${PATH:+:${PATH}}"
