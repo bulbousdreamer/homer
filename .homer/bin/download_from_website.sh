9c498646c9be5000ade04186ad04be5213fb4a32
Bulbous Dreamer <89059638+bulbousdreamer@users.noreply.github.com>
2022-09-15 22:22:09 -0500

    Add script with ways to download files from website like all the PDF notes

---
+++ .homer/bin/download_from_website.sh
@@ -0,0 +1,5 @@
+# https://stackoverflow.com/q/23446635
+
+# lftp -c 'mirror --parallel=100 http://www.something.com/afolder/ ;exit'
+
+# wget -r -np -nH --cut-dirs=3 -R index.html http://www.something.com/afolder/
