3e4f335d2988e2213317f52f626f310d5095f6df
Bulbous Dreamer <89059638+bulbousdreamer@users.noreply.github.com>
2022-09-23 13:15:29 -0500

    Add script to config name and email in all repos

---
+++ .homer/bin/git_config.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+set -u
+
+# https://mywiki.wooledge.org/BashGuide/Arrays
+
+# Set name and email in each repo
+# This is at least convenient while developing the homer repo
+# Sick of rebasing
+# TODO: Account for submodules
+
+repos=()
+while IFS= read -d '' -r; do
+	repos+=("${REPLY}")
+done < <(find "${HOME}/git" -mindepth 1 -maxdepth 1 -type d -print0)
+
+for repo in "${repos[@]}"; do
+	git -C "${repo}" config --local user.name "${1}"
+	git -C "${repo}" config --local user.email "${2}"
+
+	git submodule foreach "git -C \"${repo}\" config --local user.name \"${1}\""
+	git submodule foreach "git -C \"${repo}\" config --local user.email \"${2}\""
+done
