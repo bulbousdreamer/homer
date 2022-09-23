459792c2b26b8bbb00c1976a6f36c639ce201b89
Bulbous Dreamer <89059638+bulbousdreamer@users.noreply.github.com>
2022-09-23 13:16:47 -0500

    Remove step to make history directory because it should be tracked in Git

--- .homer/bash_profile.d/00_history.sh
+++ .homer/bash_profile.d/00_history.sh
@@ -12,7 +12,6 @@ export HISTIGNORE=$'[ \t]*:&:[fb]g:exit:ls' # Ignore the ls command as well
 # Whenever displaying the prompt, write the previous line to disk
 # export PROMPT_COMMAND="history -a"
 export HISTFILE="${HOME}/.homer/${HOMER_OS_TYPE}/history/${HOSTNAME}"
-mkdir -p "$HISTFILE"
 
 export HISTSIZE=5000
 
